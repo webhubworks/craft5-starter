4e394a3b947846375b7259520526ea769a59507a
Niklas Sonnenschein <desk@niklassonnenschein.de>
2025-02-05 21:14:48 +0100

    feat: auto install Craft

---
+++ scripts/install.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+RED='\033[1;31m'
+NC='\033[0m' # No Color
+
+if ddev craft install/check; then
+  exit 1
+fi
+
+echo -e "${YELLOW}⚠️ Craft is not installed. Running setup commands...${NC}"
+
+ddev craft setup/app-id
+ddev craft setup/security-key
+
+if ! command -v op 2>&1 >/dev/null; then
+  echo -e "${RED}1Password CLI is not installed, cannot create an account${NC}"
+  ddev craft install/craft
+else
+  read -p "What is this site called? " SITE_NAME
+
+  echo "Fetching metadata, this can take a few seconds..."
+
+  USER_EMAIL=$(op user get --format=json --me | jq -r '.email')
+  SITE_URL=$(ddev describe -j | jq -r '.raw | .primary_url')
+
+  PASSWORD=$(op item create \
+    --category=login \
+    --title="${SITE_NAME} [CMS]" \
+    --vault="Employee" \
+    --url "${SITE_URL}/admin/login" \
+    --generate-password=20,letters,digits \
+    --reveal \
+    --format json \
+    username="${USER_EMAIL}" | jq -r '.fields[] | select(.id == "password") | .value')
+
+  echo -e "${GREEN}✔ Generated a new 1Password item."
+
+  ddev craft install/craft \
+    --email="${USER_EMAIL}" \
+    --username="${USER_EMAIL}" \
+    --password="${PASSWORD}" \
+    --site-name="${SITE_NAME}" \
+    --site-url="$PRIMARY_SITE_URL" \
+    --language="de"
+fi
+
