8735a8e77a59fb73250d63a7007e3ccc3f624f40
Niklas Sonnenschein <desk@niklassonnenschein.de>
2025-02-05 14:55:03 +0100

    feat: templates

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,12 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [vue()],
+    plugins: [
+        vue(),
+        viteRestart({
+            reload: ['templates/**/*']
+        }),
+    ],
     server: {
         host: '0.0.0.0',
         port: 5137,
